9671c3335534f8b578f0575714e35b237ef48965
VIBoss <viboss1337@gmail.com>
2023-10-05 00:55:30 +0300

    Работа с переменными

--- kek/Program.cs
+++ kek/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace project {
+
+    class Program {
+
+        static void Main() {
+
+            int num_1 = 0; int num_2 = 0;
+
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("First: " + num_1 + ". Second: " + num_2);
+
+
+            //int number = -10; // Только целочисленные 
+            //uint nm10 = 10; // Только положительные числа
+            //byte bt10 = 10; // Может хранить целочисл в диапозоне -255 - +255 но при это использует в 4 раза меньше памяти чем int
+            //short sh10 = 3200; // Диапозон +-33000 и использует в 2 раза меньше памяти чем int
+            //string word = "Hello";
+            //char simvol = 's'; //Хранение 1 символа
+            //double num_2 = 5.45534; //В два раза больше места после запятой
+            //bool isHappy = true; 
+
+
+            /*Console.WriteLine(number);
+            Console.WriteLine(nm10);
+            Console.WriteLine(word);*/
+            
+        }
+
+
+    }
+
+
+}
+
+    
