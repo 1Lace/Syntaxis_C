4610e17f22516b9800a0bf88d1b9af169abc46a1
VIBoss <viboss1337@gmail.com>
2023-10-05 17:24:01 +0300

    Switch-case

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,8 +1,49 @@
-﻿
+﻿using System;
 
-int a = 5;
-int b = 6;
+namespace project {
 
-int sum = a + b;
-Console.WriteLine(sum);
+        class Program{
 
+            static void Main() {
+                short user_input = Convert.ToInt16(Console.ReadLine());
+
+                switch(user_input) {
+
+                    case 5: 
+                        System.Console.WriteLine("Number in 5"); 
+                        break;
+                    
+                    case 15: 
+                        System.Console.WriteLine("Number in 15"); 
+                        break;
+
+                     case 20: 
+                        System.Console.WriteLine("Number in 20"); 
+                        break;
+
+                     case 250: 
+                        System.Console.WriteLine("Number in 250"); 
+                        break;
+
+                    default:
+                        Console.WriteLine("Number is unrecognized!");
+                        break;
+
+                }    
+
+                
+
+
+
+
+
+
+            }
+
+            
+
+
+        }
+
+
+}
