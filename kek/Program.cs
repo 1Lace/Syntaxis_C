99c5840d1e103bcc5821398787cf4b3bb3f74e56
VIBoss <viboss1337@gmail.com>
2023-10-05 15:15:14 +0300

    Программа высчитывания плоащдь круга

--- kek/Program.cs
+++ kek/Program.cs
@@ -5,29 +5,47 @@ namespace project {
     class Program {
 
         static void Main() {
+           // System.Console.WriteLine(Math.Abs(-20)); - число по модулю
+           //Console.WriteLine(Math.Ceiling(4.11f)); // - округление к большему
+           //Console.WriteLine(Math.Floor(4.11f)); // - округление к меньшему
+            //Console.WriteLine(Math.Round(4.11f)); // - округление по ситуации
 
-            int num_1 = 0; int num_2 = 0;
+            //Console.WriteLine(Math.Min(5, 0)); // мин число
+            //Console.WriteLine(Math.Max(5, 0)); // макс число
+            //Console.WriteLine(Math.Pow(5, 2)); // возведение в степень
 
-            num_1 = Convert.ToInt32(Console.ReadLine());
-            num_2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("First: " + num_1 + ". Second: " + num_2);
-
-
-            //int number = -10; // Только целочисленные 
-            //uint nm10 = 10; // Только положительные числа
-            //byte bt10 = 10; // Может хранить целочисл в диапозоне -255 - +255 но при это использует в 4 раза меньше памяти чем int
-            //short sh10 = 3200; // Диапозон +-33000 и использует в 2 раза меньше памяти чем int
-            //string word = "Hello";
-            //char simvol = 's'; //Хранение 1 символа
-            //double num_2 = 5.45534; //В два раза больше места после запятой
-            //bool isHappy = true; 
+            System.Console.WriteLine("Введите радиус круга: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            System.Console.WriteLine("Плоащадь круга с радиусом {0} равна {1}", radius, area);
 
 
-            /*Console.WriteLine(number);
-            Console.WriteLine(nm10);
-            Console.WriteLine(word);*/
-            
+
+
+
+
+
+
+
+
+
+            //Console.Write("Введите число с точкой:" );
+            //user_input = Convert.ToDouble(Console.ReadLine());
+            //float user_input = float.Parse(Console.ReadLine());
+            //float result;
+            //result = user_input + 10f;
+            //result = result + 5f;
+            //result += 5f;
+            //result *= 2f;
+            //result++; //Добавили 1, -- - отняли единицу
+            /*result = user_input - 10f;
+            result = user_input * 10f;
+            result = user_input / 10f;
+            result = user_input % 10f;
+                                        */
+            //int a = 5, b = 6;
+            //int res = a + b;        
+            //Console.WriteLine("Переменная: " + result);
         }
 
 
