4863be611c32905c390c62d9e424d1b103bbf3a4
VIBoss <viboss1337@gmail.com>
2023-10-05 17:49:17 +0300

    добавили циклы

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -5,42 +5,44 @@ namespace project {
         class Program{
 
             static void Main() {
-                short user_input = Convert.ToInt16(Console.ReadLine());
 
-                switch(user_input) {
+                //for(float i = 200; i > 10; i /= 2 ) {
 
-                    case 5: 
-                        System.Console.WriteLine("Number in 5"); 
-                        break;
-                    
-                    case 15: 
-                        System.Console.WriteLine("Number in 15"); 
-                        break;
-
-                     case 20: 
-                        System.Console.WriteLine("Number in 20"); 
-                        break;
-
-                     case 250: 
-                        System.Console.WriteLine("Number in 250"); 
-                        break;
-
-                    default:
-                        Console.WriteLine("Number is unrecognized!");
-                        break;
-
-                }    
+               //     Console.WriteLine("Eltments: {0} ", i);
+                //byte i = 10;
+                //while(i >= 1) {
+                //    Console.WriteLine("Elements : {0}", i);
+                //    i--;
 
                 
 
+               // bool isHasCar = true;
+                //while(isHasCar) {
+                //    string end = Console.ReadLine();
+                //    if(end == "end")
+               //         isHasCar = false;
+               // }
 
+                //byte i = 100;
 
+                //do {
+                //    Console.WriteLine("Elements:" + i);
+                //} while(i < 10);sole.r
 
+                for(short i = 0; i < 10; i++) {
+                    //if(i < 5)
+                    //   break;
 
+                    if(i % 2 == 0)
+                        continue;
+                        
+                    Console.WriteLine("El:" + i);
 
-            }
+                }
 
-            
+                
+            }    
+
 
 
         }
