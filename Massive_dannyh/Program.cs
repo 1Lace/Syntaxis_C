8418913b23b35c5fde86dfed2544873a0d8ec2da
VIBoss <viboss1337@gmail.com>
2023-10-05 19:30:44 +0300

    Конспект

--- Massive_dannyh/Program.cs
+++ Massive_dannyh/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace project{
+
+    class Program {
+
+        static void Main() {
+
+            byte[] nums = new byte[5];
+            nums[0] = 250;
+            nums[1] = 50;
+            nums[2] = 20;
+            nums[3] = 100;
+            nums[4] = 25;
+
+            //Console.WriteLine("El: " + nums[0]);
+            
+
+            string[] words = new string[] {"John", "Bob", "Alex"};
+
+            words[1] = "Josh";
+
+            for(byte i = 0; i < words.Length; i++)
+                Console.WriteLine("El: " + words[i]);
+
+        }
+    }
+}
